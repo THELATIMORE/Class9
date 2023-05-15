3d53f5faa311b9a2a5f63cc37dd77308180d58b0
santiago <fernandovelandotuano69@gmail.com>
2023-05-15 18:09:53 +0200

    commit de prueba

--- ventana1.py
+++ ventana1.py
@@ -2,8 +2,9 @@ import sys
 
 from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QApplication, QFormLayout, QLineEdit, \
-    QPushButton
-from PyQt5 import QtGui
+    QPushButton, QDialog, QDialogButtonBox, QVBoxLayout
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt
 
 
 class ventana1(QMainWindow):
@@ -52,60 +53,193 @@ class ventana1(QMainWindow):
         self.ladoIzquierdo.addRow(self.letrero2)
         self.nombreCompleto = QLineEdit()
         self.nombreCompleto.setFixedWidth(250)
-        self.ladoIzquierdo.addRow("Nombre Completo", self.nombreCompleto)
+        self.ladoIzquierdo.addRow("*Nombre Completo", self.nombreCompleto)
         self.usuario = QLineEdit()
         self.usuario.setFixedWidth(250)
-        self.ladoIzquierdo.addRow("Usuario", self.usuario)
+        self.ladoIzquierdo.addRow("*Usuario", self.usuario)
         self.password = QLineEdit()
         self.password.setFixedWidth(250)
         self.password.setEchoMode(QLineEdit.Password)
-        self.ladoIzquierdo.addRow("Ingrese contraseña", self.password)
+        self.ladoIzquierdo.addRow("*Ingrese contraseña", self.password)
         self.password2 = QLineEdit()
         self.password2.setFixedWidth(250)
         self.password2.setEchoMode(QLineEdit.Password)
-        self.ladoIzquierdo.addRow("Ingrese contraseña", self.password2)
+        self.ladoIzquierdo.addRow("*Ingrese contraseña", self.password2)
         self.documento = QLineEdit()
         self.documento.setFixedWidth(250)
-        self.ladoIzquierdo.addRow("ingrese documento", self.documento)
+        self.ladoIzquierdo.addRow("*ingrese documento", self.documento)
         self.correo = QLineEdit()
         self.correo.setFixedWidth(250)
-        self.ladoIzquierdo.addRow("Ingrese Correo electronico", self.correo)
+        self.ladoIzquierdo.addRow("*Ingrese Correo", self.correo)
         self.botonRegistrar = QPushButton("Registrar")
         self.botonRegistrar.setFixedWidth(90)
         self.botonRegistrar.setStyleSheet("background-color:grey;"
-                                          "color:red;"
+                                          "color:black;"
                                           "padding: 10px;"
                                           "margin-top: 40px;")
         self.botonRegistrar.clicked.connect(self.accion_botonRegistrar)
         self.botonLimpiar = QPushButton("Limpiar")
         self.botonLimpiar.setFixedWidth(90)
         self.botonLimpiar.setStyleSheet("background-color:grey;"
-                                        "color:red;"
+                                        "color:black;"
                                         "padding: 10px;"
                                         "margin-top: 40px;")
         self.botonLimpiar.clicked.connect(self.accion_botonLimpiar)
         self.ladoIzquierdo.addRow(self.botonRegistrar,self.botonLimpiar)
-
-
-
-
-
-
-
-
-        #importante al final
+         #importante al final layout izquierdo
         self.horizontal.addLayout(self.ladoIzquierdo)
 
+        #parte derecha
+        self.ladoDerecho = QFormLayout()
+        self.ladoDerecho.setContentsMargins(100 , 0 , 0 , 0 )
+        self.letrero3 = QLabel()
+        self.letrero3.setText("Recuperar contraseña")
+        self.letrero3.setFont(QFont("Andale Mono", 20))
+        self.letrero3.setStyleSheet("color:red;")
+        self.ladoDerecho.addRow(self.letrero3)
+        self.letrero4 = QLabel()
+        self.letrero4.setFixedWidth(400)
+        self.letrero4.setText("por favor ingrese la informacion para recuperar"
+                              "\nla contraseña. los campos marcados"
+                              "\ncon asteriscos")
+        self.letrero4.setFont(QFont("Andale Mono",10))
+        self.letrero4.setStyleSheet("color: red; margin-bottom: 40px;"
+                                    "margin-top: 20px;"
+                                    "padding-bottom: 10px;"
+                                    "border: 2px solid red;"
+                                    "border-left: none;"
+                                    "border-right: none;"
+                                    "border-top: none;")
+        self.ladoDerecho.addRow(self.letrero4)
 
+        #--pregunta 1
+        self.labelPregunta1 = QLabel("                   *Pregunta de verificacion 1")
+        self.ladoDerecho.addRow(self.labelPregunta1)
+        self.pregunta1 = QLineEdit()
+        self.pregunta1.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.pregunta1)
+        self.labelRespuesta1 = QLabel("                   *Respuesta de verificacion 1")
+        self.ladoDerecho.addRow(self.labelRespuesta1)
+        self.respuesta1 = QLineEdit()
+        self.respuesta1.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.respuesta1)
+
+        #--pregunta2
+        self.labelPregunta2 = QLabel("                    *Pregunta de verificacion 2")
+        self.ladoDerecho.addRow(self.labelPregunta2)
+        self.pregunta2 = QLineEdit()
+        self.pregunta2.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.pregunta2)
+        self.labelRespuesta2 = QLabel("                   *Respuesta de verificacion 2")
+        self.ladoDerecho.addRow(self.labelRespuesta2)
+        self.respuesta2 = QLineEdit()
+        self.respuesta2.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.respuesta2)
+
+        #--pregunta3
+        self.labelPregunta3 = QLabel("                    *Pregunta de verificacion 3")
+        self.ladoDerecho.addRow(self.labelPregunta3)
+        self.pregunta3 = QLineEdit()
+        self.pregunta3.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.pregunta3)
+        self.labelRespuesta3 = QLabel("                   *Respuesta de verificacion 3")
+        self.ladoDerecho.addRow(self.labelRespuesta3)
+        self.respuesta3 = QLineEdit()
+        self.respuesta3.setFixedWidth(250)
+        self.ladoDerecho.addRow(self.respuesta3)
+        self.botonBuscar = QPushButton("Buscar")
+        self.botonBuscar.setFixedWidth(90)
+        self.botonBuscar.setStyleSheet("background-color:grey;"
+                                       "padding: 10px;"
+                                       "margin-top: 40px;")
+        self.botonRecuperar = QPushButton("Recuperar")
+        self.botonRecuperar.setFixedWidth(90)
+        self.botonRecuperar.setStyleSheet("background-color:grey;"
+                                       "padding: 10px;"
+                                       "margin-top: 40px;")
+        self.ladoDerecho.addRow(self.botonBuscar, self.botonRecuperar)
+
+        #esto al final del lado derecho
+        self.horizontal.addLayout(self.ladoDerecho)
         #esto al final
         self.fondo.setLayout(self.horizontal)
 
+        self.ventanaDialogo = QDialog(None, QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint)
+        self.ventanaDialogo.resize(300, 150)
+        self.botonAceptar = QDialogButtonBox.Ok
+        self.opciones = QDialogButtonBox(self.botonAceptar)
+        self.opciones.accepted.connect(self.ventanaDialogo.accept)
+        self.ventanaDialogo.setWindowTitle("Formulario de registro")
+        self.ventanaDialogo.setWindowModality(Qt.ApplicationModal)
+        self.vertical = QVBoxLayout()
+        self.mensaje = QLabel('')
+        self.mensaje.setStyleSheet("background-color:green; color:black; padding:10px;")
+        self.vertical.addWidget(self.mensaje)
+        self.vertical.addWidget(self.opciones)
+        self.ventanaDialogo.setLayout(self.vertical)
+
 
     def accion_botonLimpiar(self):
-        pass
-
+        self.nombreCompleto.setText('')
+        self.usuario.setText('')
+        self.password.setText('')
+        self.password2.setText('')
+        self.documento.setText('')
+        self.correo.setText('')
+        self.pregunta1.setText('')
+        self.pregunta2.setText('')
+        self.respuesta1.setText('')
+        self.respuesta2.setText('')
+        self.pregunta3.setText('')
+        self.respuesta3.setText('')
     def accion_botonRegistrar(self):
-        pass
+
+
+        self.datosCorrectos = True
+
+        if (self.password.text() != self.password2.text()):
+            self.datosCorrectos = False
+            self.mensaje.setText("los password no son iguales")
+            self.ventanaDialogo.exec_()
+
+        if (self.nombreCompleto.text() == ''
+                    or self.usuario.text() == ''
+                    or self.password.text() == ''
+                    or self.password2.text() == ''
+                    or self.documento.text() == ''
+                    or self.correo.text() == ''
+                    or self.pregunta1.text() == ''
+                    or self.pregunta2.text() == ''
+                    or self.respuesta1.text() == ''
+                    or self.respuesta2.text() == ''
+                    or self.pregunta3.text() == ''
+                    or self.respuesta3.text() == ''):
+            self.datosCorrectos = False
+            self.mensaje.setText("Debe ingresar todos los campos")
+            self.ventanaDialogo.exec_()
+        if self.datosCorrectos:
+            self.file = open('datos/clientes.txt', 'ab')
+            self.file.write(bytes(self.nombreCompleto.text() + ";"
+                                  +self.usuario.text() + ";"
+                                  +self.password.text() + ";"
+                                  +self.password2.text() + ";"
+                                  +self.documento.text() + ";"
+                                  +self.correo.text() + ";"
+                                  +self.pregunta1.text() + ";"
+                                  +self.respuesta1.text() + ";"
+                                  +self.pregunta2.text() + ";"
+                                  +self.respuesta2.text() + ";"
+                                  +self.pregunta3.text() + ";"
+                                  +self.respuesta3.text() + ";"
+                                  +"\n", encoding='UTF-8'))
+            self.file.close()
+            self.file = open('datos/clientes.txt', 'rb')
+            while self.file:
+                linea = self.file.readline().decode('UTF-8')
+                print(linea)
+                if linea == '':
+                    break
+
 
 
 
